e6d413feef1555325c3fe968b1ead91f3999db59
James Gardner <jamie.gardner25@gmail.com>
2020-10-10 13:46:53 -0700

    API Implementation (#9)
    * API Intergration
    
    * API Implementation

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react';
 import ContentEditable from 'react-contenteditable'
+import backend from '../api/backend';
 
 import './TextArea.css';
-
-const data = [ 
-    {scores: {negative: 1.0, neutral: 0.0, positive: 0.0},
-    sentiment: 'negative',
-    text: 'I am torn on the new XBOX.'},
-
-    {scores: {negative: 0.0, neutral: 0.0, positive: 1.0},
-    sentiment: 'negative',
-    text: 'It has clean, pretty lines, but it will not connect to the internet, making it useless.'}
-]
 
 class TextArea extends Component {
     constructor(props) {
@@ -25,20 +16,22 @@ class TextArea extends Component {
         this.textArea.current.focus();
     }
 
-    contentEditableChange = event => {
-        if(event.target.value.endsWith('.')) {
-            // call API
-            console.log('Called')
+    contentEditableChange = async event => {
+        const text = event.target.value.replace(/(<([^>]+)>)/gi, "").replace('&nbsp;', ' ');
+        if(text.endsWith('.') || text.endsWith('!') || text.endsWith('?')) {
+            const response = await backend.post('/finalScore', {
+                content: text
+            })
+            const data = response.data.sentences;
             var formatted = '<p>';
             for (let i = 0; i < data.length; i++) {
-                formatted = formatted + '<span style="background-color:rgb(' + data[i].scores.negative * 255 + ',' + data[i].scores.positive * 255 + ',' + data[i].scores.neutral * 255 + ')" contenteditable="false">' + event.target.value.split('. ')[i] + '</span>';
+                formatted = formatted + '<span style="background-color:rgb(' + data[i].scores.negative * 255 + ',' + data[i].scores.positive * 255 + ',' + data[i].scores.neutral * 255 + ')" contenteditable="false">' + data[i].text + ' </span> ';
             }
             formatted = formatted + '</p>';
             this.setState({ content: formatted, formattedContent: formatted });
         } else {
             this.setState({ content: event.target.value});
         }
-        console.log(this.state)
     }
 
     render() { 
